c2face701f0289c435b30db12ca15b3c125b9b44
Roland Szoke <roland.szoke@RisingStacks-MacBook-Pro.local>
2017-10-04 16:05:23 +0200

    Step_2_Solution_Roland

--- models/github/api.js
+++ models/github/api.js
@@ -1,10 +1,35 @@
 'use strict'
 
-const request = require('request-promise-native')
+const request = require('request-promise-native') // request client with Promise support
 
-function searchRepositories(query = {}) {}
+const AGENT = 'RisingStack-bootcamp'
+const URI = 'https://api.github.com'
 
-function getContributors(repository, query = {}) {}
+function searchRepositories(query = {}) {
+  return request({
+    method: 'GET',
+    uri: `${URI}/search/repositories`,
+    headers: {
+      'User-Agent': AGENT,
+      Accept: 'application/vnd.github.v3+json'
+    },
+    qs: query, // query string
+    json: true // Automatically parses the JSON string in the response
+  })
+}
+
+function getContributors(repository, query = {}) {
+  return request({
+    method: 'GET',
+    uri: `${URI}/repos/${repository}/stats/contributors`,
+    headers: {
+      'User-Agent': AGENT,
+      Accept: 'application/vnd.github.v3+json'
+    },
+    qs: query, // query string
+    json: true // Automatically parses the JSON string in the response
+  })
+}
 
 module.exports = {
   searchRepositories,
--- models/github/api.spec.js
+++ models/github/api.spec.js
@@ -1,10 +1,41 @@
 'use strict'
 
+// Nock is an HTTP mocking and expectations library for Node.js
 const nock = require('nock')
 const { expect } = require('chai')
 const api = require('./api')
 
+const AGENT = 'RisingStack-bootcamp'
+const URI = 'https://api.github.com'
+
 describe('GitHub API', () => {
-  it('should search repositories')
-  it('should get contributors')
+  it('should search repositories', async () => {
+    const response = { items: [] }
+    const gitHubAPI = nock(URI, {
+      reqheaders: {
+        'User-Agent': AGENT,
+        Accept: 'application/vnd.github.v3+json'
+      }
+    })
+      .get('/search/repositories')
+      .query({ q: 'language:javascript' })
+      .reply(200, response)
+    const result = await api.searchRepositories({ q: 'language:javascript' })
+    expect(gitHubAPI.isDone()).to.eql(true)
+    expect(result).to.eql(response)
+  })
+  it('should get contributors', async () => {
+    const response = [{ author: {}, weeks: [] }]
+    const gitHubAPI = nock(URI, {
+      reqheaders: {
+        'User-Agent': AGENT,
+        Accept: 'application/vnd.github.v3+json'
+      }
+    })
+      .get('/repos/owner/repository/stats/contributors')
+      .reply(200, response)
+    const result = await api.getContributors('owner/repository')
+    expect(gitHubAPI.isDone()).to.eql(true)
+    expect(result).to.eql(response)
+  })
 })
